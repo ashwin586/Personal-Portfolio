5c044c918abe72e38e30d19b6d76f753517af5cd
ashwin586 <ashwinv586@gmail.com>
2024-05-20 17:06:09 +0530

    Added Profile section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/components/header.css'
+import '../styles/components/profile.css';
 import '../styles/utils.css'
 
