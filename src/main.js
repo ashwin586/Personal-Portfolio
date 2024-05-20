32eb2710ad1d44da896a2ff9de51463fe7f7b22f
ashwin586 <ashwinv586@gmail.com>
2024-05-20 21:34:31 +0530

    About Section Added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/components/header.css'
-import '../styles/components/profile.css';
+import '../styles/components/hero.css';
+import '../styles/components/about.css'
 import '../styles/utils.css'
 
