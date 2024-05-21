cfa6f39bdb44600b003e137abe5e834fb487ff28
ashwin586 <ashwinv586@gmail.com>
2024-05-21 14:15:34 +0530

    Contact section completed

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import '../styles/components/header.css'
 import '../styles/components/hero.css';
 import '../styles/components/about.css'
 import '../styles/components/work.css'
+import '../styles/components/contact.css'
 import '../styles/utils.css'
 
