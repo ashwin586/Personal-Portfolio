a6103292d40397d6c17c67378762dc8d2cc4be97
ashwin586 <ashwinv586@gmail.com>
2024-05-21 15:18:25 +0530

    Mobile nav added

--- src/main.js
+++ src/main.js
@@ -6,5 +6,9 @@ import '../styles/components/about.css'
 import '../styles/components/work.css'
 import '../styles/components/contact.css'
 import '../styles/components/footer.css'
+import '../styles/components/mobile-nav.css'
 import '../styles/utils.css'
 
+import MobileNav from './utils/mobile-nav';
+
+MobileNav();
